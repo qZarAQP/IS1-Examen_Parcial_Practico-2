4ccd46f1170d77864eb245ccb6e7ea5b89f2d02a
qZarAQP <checho.laura93@gmail.com>
2017-07-06 17:18:57 -0500

    PlayLists.jsp

--- spotify/src/main/java/com/example/TransferApplication.java
+++ spotify/src/main/java/com/example/TransferApplication.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.domain.Account;
 import com.example.domain.Administrador;
 import com.example.domain.Album;
 import com.example.domain.Artista;
